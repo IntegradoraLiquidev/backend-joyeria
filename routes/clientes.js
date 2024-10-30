46a8231c015b7709c2fa280049cbc77b404af030
Ontiveroshtml <velko.lords@gmail.com>
2024-10-30 09:16:34 -0600

    fixed: eliminar 1 router.get que estaba duplicado

--- routes/clientes.js
+++ routes/clientes.js
@@ -2,13 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-    // Obtener todos los clientes
-    router.get('/', (req, res) => {
-        db.query('SELECT * FROM Cliente', (err, results) => {
-            if (err) return res.status(500).json({ error: err });
-            res.json(results);
-        });
-    });
+
     // Obtener todos los clientes
     router.get('/', (req, res) => {
         db.query('SELECT id_cliente, nombre, direccion, telefono, producto_id, quilates, precio_total, forma_pago, monto_actual, id_trabajador FROM Cliente', (err, results) => {
