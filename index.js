c67d1f80bbdacdfde0abd3e3db3f9ffceb5f515e
Ontiveroshtml <velko.lords@gmail.com>
2024-11-28 00:11:28 -0600

    Variables para produccion

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ app.use(bodyParser.json());
 
 // Configurar la conexión a la base de datos
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: '',
-    database: process.env.DB_NAME,
+    host: process.env.PROD_DB_HOST || process.env.DB_HOST,
+    user: process.env.PROD_DB_USER || process.env.DB_USER,
+    password: process.PROD_env.DB_PASSWORD || '',
+    database: process.PROD_env.DB_NAME || process.env.DB_NAME,
+    port: process.PROD_env.DB_PORT || process.env.DB_PORT || 3000,
 });
 
 // Conectar a la base de datos
@@ -42,7 +43,7 @@ app.use('/api/categorias', categoriasRoutes(db));
 app.use('/api/trabajadores', trabajadoresRoutes(db)); // Usar la nueva ruta
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.DB_PORTT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
